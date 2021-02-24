f3b1adb6291944d09b5ff447e56b0ce495713c18
lea <34993503+leazh@users.noreply.github.com>
2021-02-24 09:50:51 +0800

    Update build_vpn.sh

--- build_vpn.sh
+++ build_vpn.sh
@@ -106,7 +106,7 @@ fi
 
 if ! which docker-compose
 then 
-    curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-composei
+    curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     version=$(docker-compose --version)
